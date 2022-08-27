cfdebb857f680657b49c408b7b0235f493d661da
WeissSa <weissbsamuel@gmail.com>
2022-08-26 23:03:34 -0400

    wrote basic test for PostList

---
+++ tests/unit/PostList.spec.js
@@ -0,0 +1,26 @@
+import { shallowMount } from "@vue/test-utils";
+import PostList from "../../src/components/PostList.vue";
+import posts from "../../src/assets/posts.json";
+import Vuex from "vuex";
+
+const store = Vuex.createStore({
+  state: {
+    currentTag: null,
+  },
+});
+
+describe("PostList", () => {
+  let wrapper;
+  const getWrapper = () =>
+    shallowMount(PostList, {
+      global: {
+        plugins: [store],
+      },
+    });
+  it("should display a post for every post in posts", async () => {
+    wrapper = getWrapper();
+    posts.forEach((post) => {
+      expect(wrapper.html()).toContain(post.description);
+    });
+  });
+});
