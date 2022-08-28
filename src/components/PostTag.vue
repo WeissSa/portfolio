2a7d8a5cc4f0dabeaa718947e567deab0a9e5bef
WeissSa <weissbsamuel@gmail.com>
2022-08-28 09:18:49 -0400

    added tooltip

--- src/components/PostTag.vue
+++ src/components/PostTag.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="flex px-0 pt-1 sm:pt-4 sm:pr-1">
     <button
-      class="tag text-xs sm:text-sm px-3 rounded-2xl active:bg-blue-300 py-1 w-36 sm:w-fit"
+      data-tooltip-target="tooltip-default"
+      class="tag text-xs sm:text-sm px-3 rounded-2xl active:bg-blue-300 py-1 w-36 sm:w-fit group relative inline-block"
       :class="tagClass"
       @click="handleClick"
     >
       {{ tag }}
+      <span
+        class="absolute hidden group-hover:flex -top-2 -right-3 translate-x-full w-48 px-2 py-1 bg-gray-700 rounded-lg text-center text-white text-sm before:content-[''] before:absolute before:top-1/4 before:right-[100%] before:-translate-y-1/2 before:border-8 before:border-y-transparent before:border-l-transparent before:border-r-gray-700"
+        >Click to filter out any post unrelated to {{ tag }}. Click multiple
+        tags to broaden the filter.
+      </span>
     </button>
   </div>
 </template>
