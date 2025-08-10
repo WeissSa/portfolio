c84b2f8115ba0639a4edae937df15722cdf37035
WeissSa <weissbsamuel@gmail.com>
2025-08-10 17:23:28 -0400

    seo

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,30 @@ export default function Document() {
   return (
     <Html lang="en" {...mantineHtmlProps}>
       <Head>
+        <title>Sam Weiss{"'"}s Software Engineering Portfolio</title>
+        <meta
+          name="description"
+          content="Sam Weiss is a software engineer. Explore Sam's portfolio, featuring projects and insights on climate change, weather patterns, and environmental data visualization."
+        />
+        <meta
+          name="keywords"
+          content="Sam Weiss, Samuel Weiss, software engineer, portfolio, climate change, weather, data visualization, environmental, Toronto, historical weather"
+        />
+        <meta name="author" content="Sam Weiss" />
+        <meta
+          property="og:title"
+          content="Sam Weiss's Software Engineering Portfolio"
+        />
+        <meta
+          property="og:description"
+          content="Sam Weiss is a software engineer. Explore Sam's portfolio, featuring projects and insights on climate change, weather patterns, and environmental data visualization."
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://weisssa.github.io/portfolio" />
+        <meta
+          property="og:image"
+          content="https://weisssa.github.io/portfolio/banner.jpg"
+        />
         <ColorSchemeScript defaultColorScheme="dark" />
       </Head>
       <body>
